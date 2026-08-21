07f64c3e50b5b8395813f320faf5d8aa3fea966f
xieliang <xieliang@bopei.com.cn>
2026-08-21 10:58:49 +0800

    Initial Commit

---
+++ ClipMemo/ClipMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMemoApp.swift
+//  ClipMemo
+//
+//  Created by bigliang on 2026/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ClipMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
